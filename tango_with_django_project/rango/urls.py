23cebbfdad580c0053e7ef1d93b2a4ec74cdb086
zwelden <zach.cw@gmail.com>
2017-05-09 16:46:15 -0500

    up to pg 36 in TWD

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
+    url(r'^about/', views.about, name='about'),
     url(r'^$', views.index, name='index'),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # construct a dictionary to pass to the template engine as its context.
+    # note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunch, creamy, cookie, candy, cupcake!"}
+
+    # return a rendered response to send to the client
+    # we make use of the shortcut function to make our lives easier
+    # note that the first parameter is the template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse('Rango says here is the about page.<br><a href="/rango/">Index</a>')
